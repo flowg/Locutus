046b1e9f609120795a587426f9d3251c771faa53
FlorianGesta <flo.gesta@gmail.com>
2017-01-03 13:35:56 +0100

    saving conf in a .yo-rc.json, composing with the requested sub-generator and creating a package.json for any subgen

--- generators/app/index.js
+++ generators/app/index.js
@@ -11,27 +11,14 @@ class Locutus extends Generator {
         // Calling the super constructor is important so our generator is correctly set up
         super(args, opts);
 
-        /**
-         * Next, add your custom code
-         */
-
-        // This method adds support for a `--coffee` flag
-        this.option('coffee');
-
-        // This makes `appname` a required argument.
-        //this.argument('appname', { type: String, required: true });
-
-        // And you can then access it later
-        this.log(this.options.appname);
+        // Initializing class attributes
+        this.configuration = {};
     }
 
     /**
      * Methods considered as tasks, which will automatically be run in sequence
      * when calling the generator.
      */
-    method1() {
-        //console.log('method 1 just ran'); // Wrong : always use the abstracted this.log()
-    }
 
     // Your initialization methods (checking current project state, getting configs, etc)
     initializing() {}
@@ -63,56 +50,47 @@ class Locutus extends Generator {
                 }
             ]
         ).then((answers) => {
-            this.log('app name', answers.name);
-            this.log('app type', answers.appType);
+            this.configuration = {
+                appName: answers.appName,
+                appType: answers.appType
+            }
         });
     }
 
     // Saving configurations and configure the project (creating .editorconfig files and other metadata files)
-    configuring() {}
+    configuring() {
+        this.config.set(this.configuration);
+    }
 
     // If the method name doesn't match a priority, it will be pushed to this group
     default() {
-        // Plug in another generator
-        this.composeWith(require.resolve('../web'));
+        // Compose with the requested sub-generator
+        let path = `../${this.configuration.appType}`;
+        this.composeWith(require.resolve(path));
     }
 
     // Where you write the generator specific files (see http://yeoman.io/authoring/file-system.html)
-    writing() {
-        // Copying a file with a <%= title %> placeholder in it
-        // The template root is by default ./templates/
-        // The destination root is where the end user will bootstrap its project
-        this.fs.copyTpl(
-            this.templatePath('index.html'),
-            this.destinationPath('public/index.html'),
-            { title: 'Templating with Yeoman' }
-        );
-    }
+    writing() {}
 
     // Where conflicts about overwriting pre-existing files are handled (used internally)
     conflicts() {}
 
     // Where installation are run (npm, bower)
     install() {
-        // Use a normal Bash command (normalizing command so it can run on any platform)
-        // You can use the asynchronous version too, named spawnCommand()
+        // Initializing the package.json file, common to all app types
         this.spawnCommandSync('npm', ['init']);
-
-        // Use this to install an NPM module as a DevDependency
-        this.npmInstall(['lodash'], { 'save-dev': true });
     }
 
     // Called last, cleanup, say good bye, etc
-    end() {}
+    end() {
+        this.log('Assimilation complete');
+    }
 
     /**
      * Methods considered as private from the generator run loop's point of view.
      * They have to be called explicitly.
      * @private
      */
-    _private_method() {
-        this.log('private hey');
-    }
 }
 
 module.exports = Locutus;
--- generators/web/index.js
+++ generators/web/index.js
@@ -11,46 +11,20 @@ class LocutusWeb extends Generator {
     constructor(args, opts) {
         // Calling the super constructor is important so our generator is correctly set up
         super(args, opts);
-
-        // Next, add your custom code
-        this.option('coffee'); // This method adds support for a `--coffee` flag
-
-        // This makes `appname` a required argument.
-        //this.argument('appname', { type: String, required: true });
-
-        // And you can then access it later
-        this.log(this.options.appname);
     }
 
     /**
      * Methods considered as tasks, which will automatically be run in sequence
      * when calling the generator.
      */
-    method1() {
-        //console.log('method 1 just ran'); // Wrong : always use the abstracted this.log()
-    }
 
     // Your initialization methods (checking current project state, getting configs, etc)
     initializing() {
-        this.log('Welcome into the web subgenerator');
+        this.log('Web app assimilation process has begun');
     }
 
     // Where you prompt users for options (where you'd call this.prompt())
-    prompting() {
-        return this.prompt([{
-            type    : 'input',
-            name    : 'name',
-            message : 'Your project name',
-            default : this.appname // Default to current folder name
-        }, {
-            type    : 'confirm',
-            name    : 'cool',
-            message : 'Would you like to enable the Cool feature?'
-        }]).then((answers) => {
-            this.log('app name', answers.name);
-            this.log('cool feature', answers.cool);
-        });
-    }
+    prompting() {}
 
     // Saving configurations and configure the project (creating .editorconfig files and other metadata files)
     configuring() {}
@@ -75,9 +49,6 @@ class LocutusWeb extends Generator {
      * They have to be called explicitly.
      * @private
      */
-    _private_method() {
-        this.log('private hey');
-    }
 }
 
 module.exports = LocutusWeb;
